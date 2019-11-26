cbafffb0cd08cbc5ef8a656bf64352ec9c07fb5a
Longze SU <lsu018@ucr.edu>
2019-11-25 19:51:03 -0800

    1st :balloon:

--- sparkSQL/src/main/java/edu/ucr/cs/cs226/SparkSQL.java
+++ sparkSQL/src/main/java/edu/ucr/cs/cs226/SparkSQL.java
@@ -1,13 +1,12 @@
 package edu.ucr.cs.cs226;
 
-
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
-import org.apache.spark.sql.functions.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +20,7 @@ public class SparkSQL {
                 // .config("")
                 .getOrCreate();
 
+        /*spark.sqlContext().implicits();*/
         // Schema
         String schema = "host tmp timestamp ins url code bytes";
         List<StructField> fields = new ArrayList<>();
@@ -38,11 +38,45 @@ public class SparkSQL {
                 .option("delimiter", "\t")
                 .schema(sch)
                 .load(input);
-        // demon
-        df.show(5);
-
         // operation
         // host 0; - 1; timestamp 2; ins 3; url 4; code 5; bytes 6
-        df.groupBy("code").avg("bytes");
+        df.createOrReplaceTempView("visit");
+        Dataset<Row> avgBytes = df.sqlContext().sql("SELECT code, avg(bytes) as num from visit");
+        avgBytes.show(5);
+        // df.groupBy("code").avg("bytes");
+        JavaRDD<String> res = avgBytes.toJavaRDD().map(s -> "Code " + s.getAs("code").toString() + ", average number of bytes = " + s.getAs("num").toString());
+        res.saveAsTextFile(output);
+    }
+
+    public void findPair(String input, String output, long t0, long t1) {
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL")
+                // .config("")
+                .getOrCreate();
+
+        /*spark.sqlContext().implicits();*/
+        // Schema
+        String schema = "host tmp time ins url code bytes";
+        List<StructField> fields = new ArrayList<>();
+        for (String fieldName : schema.split(" ")) {
+            StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType,true);
+            fields.add(field);
+        }
+        fields.set(2, DataTypes.createStructField("time", DataTypes.LongType, true));
+        // fields.set(6, DataTypes.createStructField("bytes", DataTypes.DoubleType, true));
+        StructType sch = DataTypes.createStructType(fields);
+
+        Dataset<Row> df = spark.read()
+                .format("csv")
+                .option("header", "false")
+                .option("delimiter", "\t")
+                .schema(sch)
+                .load(input);
+        // operation
+        // host 0; - 1; timestamp 2; ins 3; url 4; code 5; bytes 6
+        Dataset<Row> res = df.select("time").filter("time > t0 and time < t1");
+        long time_count = df.count();
+        System.out.println("Number of time between t0 and t1 are "+time_count);
     }
 }
