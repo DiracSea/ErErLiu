5b2c9ab86e3fd40949c29608ed2301c080c746f1
Longze SU <lsu018@ucr.edu>
2019-11-27 01:41:09 -0800

    TFIDF

---
+++ TFIDF/src/main/java/TFIDF/single.java
@@ -0,0 +1,102 @@
+package TFIDF;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.feature.HashingTF;
+import org.apache.spark.mllib.feature.IDF;
+import org.apache.spark.mllib.feature.IDFModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+
+public class single {
+
+    public String getValue(String path, String src) {
+//        SparkConf conf = new SparkConf();
+//        conf.setAppName("WordCounter").setMaster("local");
+//        JavaSparkContext sc = new JavaSparkContext(conf);
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("TF.IDF")
+                // .config("")
+                .getOrCreate();
+
+        final HashingTF hashingTF = new HashingTF();
+
+		/*
+		 	hello mllib
+			spark
+			goodBye spark
+			hello spark
+			goodBye spark
+		 */
+		Dataset<Row> df = spark.read().json(path+"/"+src+"/COMMENTS_"+src+".json").select("body");
+        df.show(5);
+		JavaRDD<String> text = df.toJavaRDD().map(s -> s.getAs("body").toString());
+
+        JavaRDD<Vector> tf = text.map(new Function<String, Vector>() {
+
+            @Override
+            public Vector call(String v1) throws Exception {
+                return hashingTF.transform(Arrays.asList(v1.replaceAll("[^a-zA-Z\\s]", "").split(" ")));
+            }
+        });
+        IDFModel idf = new IDF().fit(tf);
+
+        JavaRDD<Vector> tfIdf = idf.transform(tf);
+
+        List<Vector> list = tfIdf.collect();
+        spark.close();
+        return list.toString();
+    }
+
+    public String[] findDir(String path) {
+        File file = new File(path);
+        String[] directories = file.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File current, String name) {
+                return new File(current, name).isDirectory();
+            }
+        });
+        System.out.println(Arrays.toString(directories));
+        return directories;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException {
+        String input = args[0], output = args[1];
+        single s = new single();
+        String[] dir = s.findDir(input);
+
+        boolean append = true;
+        boolean autoFlush = true;
+        String charset = "UTF-8";
+        String filePath = output;
+        String tmp;
+
+        File file = new File(filePath);
+        FileOutputStream fos;
+        OutputStreamWriter osw;
+        BufferedWriter bw;
+        PrintWriter pw;
+
+
+        for (String d : dir) {
+            if(!file.getParentFile().exists()) file.getParentFile().mkdirs();
+            fos = new FileOutputStream(file, append);
+            osw = new OutputStreamWriter(fos, charset);
+            bw = new BufferedWriter(osw);
+            pw = new PrintWriter(bw, autoFlush);
+            pw.write(d);
+            tmp = s.getValue(input,d);
+            pw.write(tmp);
+        }
+    }
+}
--- sparkSQL/readme.md
+++ sparkSQL/readme.md
@@ -1,3 +1,17 @@
+# sparkSQL
 
+## run 
+sudo spark-submit --class edu.ucr.cs.cs226.SparkSQL target/lsu018-1.0-SNAPSHOT.jar /data/input/nasa.tsv /data/output/sql /data/output/t 800000000 804572208
 
+## results
+Code 200, average number of bytes = 22848.716743119265
+Code 302, average number of bytes = 73.54901960784314
+Code 404, average number of bytes = 0.0
+Code 304, average number of bytes = 0.0
+
+Logs between time0 and time1 are 881
+
+Logs between time0 and time1 are 881
+
+## time 
 time1: 17774.729675; time2: 1552.986433; time3: 16.984
