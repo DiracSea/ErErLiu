2da4a9586647ee0e02dbccdf11d4345073f763c7
Longze SU <lsu018@ucr.edu>
2019-11-30 01:58:44 -0800

    test2

--- TFIDF/src/main/java/TFIDF/single.java
+++ TFIDF/src/main/java/TFIDF/single.java
@@ -38,9 +38,9 @@ public class single {
                 .json(path+"/"+src+"/COMMENTS_"+src+".json")
                 .select("body");
         Dataset<Row> new_df = df
-                .withColumn("body", functions.regexp_replace(df.col("body"), "[^a-zA-Z.'?!]", " "))
-                .filter("body != '[deleted]'")
-                .filter("body != ''"); // remove strange symbols include 0-9,.?!
+                // .withColumn("body", functions.regexp_replace(df.col("body"), "[^a-zA-Z.'?!]", " "))
+                .select(functions.regexp_replace(df.col("body"),"[^a-zA-Z.']+"," "))
+                .filter(df.col("body").notEqual("[deleted]").notEqual("").notEqual(" ")); // remove strange symbols include 0-9,.?!
         new_df.show(5);
 
         Tokenizer tokenizer = new Tokenizer()
