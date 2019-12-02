e9f1e5b24a59ea412eb3b807fbddd2cbea74a6e7
Longze SU <lsu018@ucr.edu>
2019-12-02 08:33:41 -0800

    321:

--- TFIDF/src/main/java/Visualization/wordCloud.java
+++ TFIDF/src/main/java/Visualization/wordCloud.java
@@ -1,12 +1,12 @@
 package Visualization;
 
-import com.kennycason.kumo.*;
-import com.kennycason.kumo.bg.CircleBackground;
-import com.kennycason.kumo.bg.PixelBoundryBackground;
-import com.kennycason.kumo.font.scale.LinearFontScalar;
-import com.kennycason.kumo.font.scale.SqrtFontScalar;
-import com.kennycason.kumo.nlp.FrequencyAnalyzer;
-import com.kennycason.kumo.palette.ColorPalette;
+//import com.kennycason.kumo.*;
+//import com.kennycason.kumo.bg.CircleBackground;
+//import com.kennycason.kumo.bg.PixelBoundryBackground;
+//import com.kennycason.kumo.font.scale.LinearFontScalar;
+//import com.kennycason.kumo.font.scale.SqrtFontScalar;
+//import com.kennycason.kumo.nlp.FrequencyAnalyzer;
+//import com.kennycason.kumo.palette.ColorPalette;
 import main.all;
 import main.single;
 import org.apache.spark.api.java.JavaRDD;
@@ -43,35 +43,35 @@ public class wordCloud {
             df1.toJSON().toJavaRDD().repartition(1).saveAsTextFile(output+"/"+d);
         }
     }
-    public void bi() throws IOException {
-        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-        frequencyAnalyzer.setWordFrequenciesToReturn(750);
-        frequencyAnalyzer.setMinWordLength(4);
-        // frequencyAnalyzer.setStopWords(loadStopWords());
-
-        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("text/new_york_positive.txt");
-        final List<WordFrequency> wordFrequencies2 = frequencyAnalyzer.load("text/new_york_negative.txt");
-        final Dimension dimension = new Dimension(600, 600);
-        final PolarWordCloud wordCloud = new PolarWordCloud(dimension, CollisionMode.PIXEL_PERFECT, PolarBlendMode.BLUR);
-        wordCloud.setPadding(2);
-        wordCloud.setBackground(new CircleBackground(300));
-        wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
-        wordCloud.build(wordFrequencies, wordFrequencies2);
-        wordCloud.writeToFile("~/output/polar_newyork_circle_blur_sqrt_font.png");
-    }
-    public void mon() throws IOException {
-        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-        frequencyAnalyzer.setWordFrequenciesToReturn(300);
-        frequencyAnalyzer.setMinWordLength(4);
-
-        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("text/datarank.txt");
-        final Dimension dimension = new Dimension(500, 312);
-        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
-        wordCloud.setPadding(2);
-        wordCloud.setBackground(new PixelBoundryBackground("backgrounds/whale_small.png"));
-        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
-        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
-        wordCloud.build(wordFrequencies);
-        wordCloud.writeToFile("~/output/whale_wordcloud_small.png");
-    }
+//    public void bi() throws IOException {
+//        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+//        frequencyAnalyzer.setWordFrequenciesToReturn(750);
+//        frequencyAnalyzer.setMinWordLength(4);
+//        // frequencyAnalyzer.setStopWords(loadStopWords());
+//
+//        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("text/new_york_positive.txt");
+//        final List<WordFrequency> wordFrequencies2 = frequencyAnalyzer.load("text/new_york_negative.txt");
+//        final Dimension dimension = new Dimension(600, 600);
+//        final PolarWordCloud wordCloud = new PolarWordCloud(dimension, CollisionMode.PIXEL_PERFECT, PolarBlendMode.BLUR);
+//        wordCloud.setPadding(2);
+//        wordCloud.setBackground(new CircleBackground(300));
+//        wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+//        wordCloud.build(wordFrequencies, wordFrequencies2);
+//        wordCloud.writeToFile("~/output/polar_newyork_circle_blur_sqrt_font.png");
+//    }
+//    public void mon() throws IOException {
+//        final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+//        frequencyAnalyzer.setWordFrequenciesToReturn(300);
+//        frequencyAnalyzer.setMinWordLength(4);
+//
+//        final List<WordFrequency> wordFrequencies = frequencyAnalyzer.load("text/datarank.txt");
+//        final Dimension dimension = new Dimension(500, 312);
+//        final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+//        wordCloud.setPadding(2);
+//        wordCloud.setBackground(new PixelBoundryBackground("backgrounds/whale_small.png"));
+//        wordCloud.setColorPalette(new ColorPalette(new Color(0x4055F1), new Color(0x408DF1), new Color(0x40AAF1), new Color(0x40C5F1), new Color(0x40D3F1), new Color(0xFFFFFF)));
+//        wordCloud.setFontScalar(new LinearFontScalar(10, 40));
+//        wordCloud.build(wordFrequencies);
+//        wordCloud.writeToFile("~/output/whale_wordcloud_small.png");
+//    }
 }
