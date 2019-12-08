51c046718e1ced4e65d67a02aa14d79ec279616c
Longze SU <lsu018@ucr.edu>
2019-12-08 13:00:06 -0800

    123

--- TFIDF/src/main/java/kmeans/chooseNum.java
+++
@@ -1,4 +0,0 @@
-package kmeans;
-
-public class chooseNum {
-}
--- TFIDF/src/main/java/kmeans/result.java
+++ TFIDF/src/main/java/kmeans/result.java
@@ -8,7 +8,6 @@ public class result {
         String input = args[0], cluster = args[1], output = args[2], output1 = args[3];
         int c = Integer.parseInt(cluster), best_i = 0, best_j = 0;
         double res, best = Double.POSITIVE_INFINITY;
-        int[] iterate = {30, 50, 100};
         kmeans k = new kmeans();
         PrintWriter pw = null;
         try {
@@ -17,19 +16,16 @@ public class result {
             e.printStackTrace();
         }
         StringBuilder builder = new StringBuilder();
-        String ColumnNamesList = "cluster number,iterate number,WSSSE";
-// No need give the headers Like: id, Name on builder.append
-        builder.append(ColumnNamesList + "\n");
+//        String ColumnNamesList = "cluster number,iterate number,WSSSE";
+//// No need give the headers Like: id, Name on builder.append
+//        builder.append(ColumnNamesList + "\n");
         for (int i = 1; i < c; i++) {
-            for (int j : iterate){
-                res = k.run_cost(input, i, j);
-                if (res < best) {
-                    best = res;
-                    best_i = i;
-                    best_j = j;
-                }
-                builder.append(String.valueOf(i)+","+String.valueOf(j)+","+res+"\n");
-                pw.write(builder.toString());
+            res = k.run_cost(input, i, 50);
+            if (res < best) {
+                best = res;
+                best_i = i;
+            builder.append(String.valueOf(i)+","+res+"\n");
+            pw.write(builder.toString());
             }
         }
 
