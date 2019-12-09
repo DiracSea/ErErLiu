86ca5125f09db19c21bf63df586e2e295dcaffed
Longze SU <lsu018@ucr.edu>
2019-12-09 13:48:36 -0800

    dataset

--- TFIDF/src/main/java/kmeans/kmeans.java
+++ TFIDF/src/main/java/kmeans/kmeans.java
@@ -1,35 +1,35 @@
 package kmeans;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.spark.SparkSession;
+
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.mllib.clustering.KMeansModel;
 import org.apache.spark.mllib.linalg.Vector;
 import org.apache.spark.mllib.linalg.Vectors;
 import org.apache.spark.mllib.clustering.KMeans;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+import org.apache.spark.sql.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Serializable;
 
 
 public class kmeans {
-    private static SparkSession sc = null;
+    private static SparkSession spark = null;
     public static SparkSession initSC() {
-        if (sc == null) {
-            sc = new SparkSession
+        if (spark == null) {
+            spark = SparkSession
                     .builder()
                     .appName("KMeans")
                     .getOrCreate();
         }
-        return sc;
+        return spark;
     }
     public double run_cost(String input, int num_cluster, int iter) {
         SparkSession spark = initSC();
-        JavaSparkContext jsc = new JavaSparkContext(spark);
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
         // Load and parse data
         JavaRDD<String> data = jsc.textFile(input);
         JavaRDD<Vector> parsedData = data.map(s -> {
@@ -54,10 +54,33 @@ public class kmeans {
 
         return SE;
     }
+    public static class Line1 implements Serializable {
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        private String value;
+
+    }
+    public static class Line2 implements Serializable {
+        public int getLabel() {
+            return label;
+        }
+
+        public void setLabel(int label) {
+            this.label = label;
+        }
+
+        private int label;
+    }
 
     public void run_kmeans(String input, int num_cluster, int iter, String output) {
         SparkSession spark = initSC();
-        JavaSparkContext jsc = new JavaSparkContext(spark);
+        JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
         // Load and parse data
         JavaRDD<String> data = jsc.textFile(input);
         JavaRDD<Vector> parsedData = data.map(s -> {
@@ -78,9 +101,18 @@ public class kmeans {
         // Evaluate clustering by computing Within Set Sum of Squared Errors
         JavaRDD<Integer> res = clusters.predict(parsedData);
 
-        Dataset<Row> df = parsedData.toDF("value");
+        Dataset<Row> df1 = spark
+                .createDataFrame(parsedData.map(s -> s.toString()), Line1.class)
+                .withColumn("id", functions.monotonically_increasing_id());
+        Dataset<Row> df2 = spark
+                .createDataFrame(res, Line2.class)
+                .withColumn("id", functions.monotonically_increasing_id());
 
-        JavaRDD<String> com = df.union(res.toDF("label")).toJavaRDD.coalesce(1);
+        Dataset<Row> df = df1
+                .join(df2,  df1.col("id").equalTo(df2.col("id")))
+                .drop("id");
+
+        JavaRDD<String> com = df.as(Encoders.STRING()).toJavaRDD().coalesce(1);
 
         com.saveAsTextFile(output);
         // smallest is best
